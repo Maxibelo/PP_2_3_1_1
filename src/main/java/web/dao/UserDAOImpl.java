b5505867fd074d97085ee32787c7b1b738ae5901
Maxibelo <maxibelo78@yandex.ru>
2023-05-12 22:59:22 +0300

    Initial commit

---
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -0,0 +1,44 @@
+package web.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Autowired
+    public UserDAOImpl() {
+    }
+
+    @Override
+    public List getAllUsers() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        entityManager.remove(entityManager.find(User.class, user.getId()));
+    }
+
+    @Override
+    public void edit(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User getById(int id) {
+        return entityManager.find(User.class, id);
+    }
+}
